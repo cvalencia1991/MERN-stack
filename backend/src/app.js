e548d46086f59040f13b1097056eda30c6036d30
Cesar Valencia <cesar4a6z@gmail.com>
2023-06-13 21:50:16 -0500

    Add Connection with the views

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,25 @@
 import express from "express";
 import indexRoutes from "./routes/index.routes";
+import { engine } from "express-handlebars";
+import  path from "path";
 
 const app = express();
 
+app.set("views", path.join(__dirname, "./views"));
+
+app.engine(
+  ".hbs",
+  engine({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),	
+    extname: ".hbs",
+  })
+);
+
+app.set("view engine", ".hbs");
+
+
+// Routes
 app.use(indexRoutes);
 
 export default app;
--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -3,11 +3,11 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render('home');
 });
 
 router.get("/about", (req, res) => {
-  res.send("about");
+  res.send('about');
 });
 
 export default router;
