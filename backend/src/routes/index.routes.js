b845dc88367a4e9dd480bfd82eb09ff8d500e4ab
Cesar Valencia <cesar4a6z@gmail.com>
2023-06-18 20:33:19 -0500

    Toogle Direction

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -16,11 +16,39 @@ router.post("/tasks/add", async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-  //res.send('save-task');
 });
 
 router.get("/about", (req, res) => {
   res.render("about");
+});
+
+router.get("/edit/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id).lean();
+    res.render("edit", { task });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+router.post("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  await Task.findByIdAndUpdate(id, req.body);
+  res.redirect("/");
+});
+
+router.get("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  await Task.findByIdAndDelete(id);
+  res.redirect("/");
+});
+
+router.get("/toggleDone/:id", async (req, res) => {
+  const {id} = req.params;
+  const task = await Task.findById(id);
+  task.done = !task.done;
+  await task.save();
+  res.redirect("/");
 });
 
 export default router;
